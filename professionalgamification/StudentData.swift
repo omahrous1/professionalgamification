51c5e1e69a50ccfc9c0748a20f2a2cba05b14fff
JacobHarrison1004 <31573082+JacobHarrison1004@users.noreply.github.com>
2017-12-15 16:51:03 -0600

    Created Segues between all screens

---
+++ professionalgamification/StudentData.swift
@@ -0,0 +1,16 @@
+//
+//  StudentData.swift
+//  professionalgamification
+//
+//  Created by Jacob Harrison on 12/15/17.
+//  Copyright © 2017 Maryville App Development. All rights reserved.
+//
+
+import Foundation
+
+struct StudentData
+{
+    var studentName: String
+    var studentEmail: String
+    var studentPoints: Int
+}
